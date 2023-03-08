d9f967b88d33050c9421259ebc25fccaa617933d
de-only-wei <65906926+de-only-wei@users.noreply.github.com>
2023-03-08 14:17:01 -0800

    Initial commit

---
+++ src/Config.js
@@ -0,0 +1,34 @@
+import { SunIcon, MoonIcon, PencilIcon } from "@heroicons/react/outline";
+
+import { routes } from "./routes";
+
+// app config
+
+export const config = {
+	title: "John Doe",
+	home: routes.home,
+	pages: [routes.projects],
+	themes: [
+		{
+			name: "Light",
+			id: "winter",
+			icon: <SunIcon className="h-6 w-6" />,
+		},
+		{
+			name: "Dark",
+			id: "dark",
+			icon: <MoonIcon className="h-6 w-6" />,
+		},
+		{
+			name: "Retro",
+			id: "retro",
+			icon: <PencilIcon className="h-6 w-6" />,
+		}
+	],
+	masonryColumns: {
+		default: 4,
+		960: 3,
+		730: 2,
+		500: 1,
+	},
+};
